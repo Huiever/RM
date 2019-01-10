a3d2eb7678d3fe0d451ca5f3b06a554c7ec1ff65
Huiever <Huiever@outlook.com>
2019-01-10 14:56:05 +0800

    6050_dmp and wt931 done!

---
+++ external_imu/6050_dmp/USER/main.h
@@ -0,0 +1,12 @@
+#ifndef __MAIN_H__
+#define __MAIN_H__
+#include "sys.h"
+#include "delay.h"
+#include "usart.h"
+#include "led.h"
+#include "mpu6050.h"
+#include "inv_mpu.h"
+#include "inv_mpu_dmp_motion_driver.h" 
+#include "visualscope.h"
+
+#endif
